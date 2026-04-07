179b466388c48c9caa6e8829a5727a0e8ec57345
Huzaifa <huzikhan123456@gmail.com>
2026-04-07 19:03:49 +0500

    Speedup: reduce scroll duration from 1.2s to 0.5s for snappier feel

--- src/hooks/useLenis.js
+++ src/hooks/useLenis.js
@@ -4,7 +4,7 @@ import Lenis from 'lenis'
 const useLenis = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: 'vertical',
       gestureDirection: 'vertical',
