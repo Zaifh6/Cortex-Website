f6b390bec594df19f5ee6b2dc7bd2b357b0e9bb4
Huzaifa <huzikhan123456@gmail.com>
2026-04-07 19:05:33 +0500

    Speedup: reduce scroll duration to 0.3s for lightning-fast scrolling

--- src/hooks/useLenis.js
+++ src/hooks/useLenis.js
@@ -4,7 +4,7 @@ import Lenis from 'lenis'
 const useLenis = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 0.5,
+      duration: 0.3,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       direction: 'vertical',
       gestureDirection: 'vertical',
